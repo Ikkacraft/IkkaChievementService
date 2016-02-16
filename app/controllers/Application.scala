0ffefeacc7d22816b5293bf092e67697edfe28e0
CelyaRousseau <celyarousseau.pro@gmail.com>
2016-02-16 20:04:30 +0100

    add security support

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,11 @@
 package controllers
 
+import Actions.Authorized
 import play.api.mvc._
 
 class Application extends Controller {
 
-  def index = Action {
+  def index = Authorized {
     Ok("Your new application is ready.")
   }
 
